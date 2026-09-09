3f8cc5df9f6880c18e15263226c30d8c8b4b1039
Kusuma Adhimaya <kusumaputra2007@gmail.com>
2026-09-09 23:13:54 +0700

    Tutorial 02

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Kusuma Putra Abdillah Adhimaya",
+        "npm": "2506656936",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "Mahasiswa Ilmu Komputer Universitas Indonesia yang tertarik "
+            "pada web development."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Kusuma Putra Abdillah Adhimaya",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
