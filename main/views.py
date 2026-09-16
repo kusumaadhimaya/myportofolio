61d947b784e85732fa8b4018c77ab9653a9567dc
Kusuma Adhimaya <kusumaputra2007@gmail.com>
2026-09-16 23:42:02 +0700

    Tutorial 02

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 from main.models import Experience, Skill
+from main.forms import SkillForm
 
 def show_main(request):
     context = {
@@ -23,8 +27,53 @@ def show_experience(request):
     return render(request, "experience.html", context)
 
 def show_skills(request):
+    json_response = get_skills_json(request)
+
+    skills = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    skills = [skill.object for skill in skills]
+    title_query = request.GET.get("title", "").strip()
+
     context = {
         "name": "Kusuma Putra Abdillah Adhimaya",
-        "skills_list": Skill.objects.all(),
+        "skills_list": skills,
+        "title_query": title_query,
     }
     return render(request, "skills.html", context)
+
+def create_skill(request):
+    form = SkillForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect("main:show_skills")
+
+    context = {
+        "name": "Kusuma Putra Abdillah Adhimaya",
+        "form": form,
+    }
+
+    return render(request, "create_skill.html", context)
+
+def get_skills_json(request):
+    title_query = request.GET.get("title", "").strip()
+    skills = Skill.objects.all()
+
+    if title_query:
+        skills = skills.filter(title__icontains=title_query)
+
+    skills_json = serializers.serialize("json", skills)
+    return HttpResponse(skills_json, content_type="application/json")
+
+def delete_skill(request, skill_id):
+    skill = get_object_or_404(Skill, pk=skill_id)
+
+    if request.method == "POST":
+        skill.delete()
+        messages.success(request, "Skill berhasil dihapus!")
+        return redirect("main:show_skills")
+
+    return redirect("main:show_skills")
